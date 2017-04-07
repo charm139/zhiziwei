b17593738c3bebccf3566e3d208c1a10a4b8bed6
quanliang <838453790@qq.com>
2017-04-07 15:56:21 +0800

    简单轮廓

--- pages/classify/index.js
+++ pages/classify/index.js
@@ -22,25 +22,7 @@ Page({
         curNav: 1,
 		curIndex: 0
     },
-    // onLoad: function() {
 
-    //     var that = this
-        
-    //     wx.request({
-    //         url: 'http://huanqiuxiaozhen.com/wemall/goodstype/typebrandList',
-    //         method: 'GET',
-    //         data: {},
-    //         header: {
-    //             'Accept': 'application/json'
-    //         },
-    //         success: function(res) {
-    //             that.setData({
-    //                 navLeftItems: res.data,
-    //                 navRightItems: res.data
-    //             })
-    //         }
-    //     })
-    // },
 
     //事件处理函数
     // switchRightTab: function(e) {
